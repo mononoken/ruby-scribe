a3b9ed0036dae668b02a3a250dcc11dbf9ce9166
mononoken <ken.maeshima@pm.me>
2023-07-11 03:30:42 -0700

    Add test for signed in user viewing /notes

--- spec/system/notes_spec.rb
+++ spec/system/notes_spec.rb
@@ -4,15 +4,27 @@ RSpec.describe "Notes", type: :system do
   describe "viewing notes" do
     let(:user) { create(:user) }
 
-    context "when user is logged out" do
+    context "when user is signed out" do
       before do
-        sign_out :user
+        sign_out user
       end
 
       it "redirects to login" do
         visit notes_path
 
         expect(page).to have_current_path(user_session_path)
+      end
+    end
+
+    context "when user is signed in" do
+      before do
+        sign_in user
+      end
+
+      it "shows notes" do
+        visit notes_path
+
+        expect(page).to have_current_path(notes_path)
       end
     end
   end
