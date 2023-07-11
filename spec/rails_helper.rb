5a1b6d2fedfdba3c43c4f1cc252a94beba8c9132
mononoken <ken.maeshima@pm.me>
2023-07-10 22:57:52 -0700

    Setup RSpec support for FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,8 @@ require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Use capybara for system tests
-require "capybara/rspec"
-require "webdrivers"
+# require "capybara/rspec"
+# require "webdrivers"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
