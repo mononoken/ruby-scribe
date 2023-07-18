7dd4636b6a06df665fff6acaea8c2c19c26d315e
mononoken <ken.maeshima@pm.me>
2023-07-17 23:29:32 -0700

    Create some validation specs for Campaign model

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,38 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Delete if failing
+  context "when campaign has a name" do
+    subject(:campaign) { described_class.new(name: "Test Campaign") }
+
+    it "is valid" do
+      expect(campaign.valid?).to be(true)
+    end
+
+    context "when campaign has no user" do
+      fit "is not valid" do
+        expect(campaign.valid?).to be(false)
+      end
+    end
+
+    context "when campaign has a user" do
+      let(:user) { create(:user) }
+      subject(:campaign) do
+        described_class.new(name: "Test Campaign", user_id: user.id)
+      end
+
+      fit "is valid" do
+        expect(campaign.valid?).to be(true)
+      end
+    end
+  end
+
+  # Delete if failing
+  context "when campaign has no name" do
+    subject(:campaign) { described_class.new(name: nil) }
+
+    it "is not valid" do
+      expect(campaign.valid?).to be(false)
+    end
+  end
 end
