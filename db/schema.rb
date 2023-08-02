e611cb4258a23b36daa54b67ed4ea1d06b4d10ba
mononoken <ken.maeshima@pm.me>
2023-08-01 23:13:38 -0700

    Generate Note model and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_035116) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_060402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_035116) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_journals_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
