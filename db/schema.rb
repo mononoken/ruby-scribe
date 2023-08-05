9f5d525c3daea01ae43cd4aa204d31379287866f
mononoken <ken.maeshima@pm.me>
2023-08-05 01:02:46 -0700

    Generate and migrate migration for journal reference in notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_02_060402) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_075956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_060402) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "journal_id", null: false
+    t.index ["journal_id"], name: "index_notes_on_journal_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_02_060402) do
   end
 
   add_foreign_key "journals", "users"
+  add_foreign_key "notes", "journals"
 end
