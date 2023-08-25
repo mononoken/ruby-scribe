216ab966a8e9a222193b3c1bae1584f1e056f2cb
mononoken <ken.maeshima@pm.me>
2023-08-24 17:41:15 -0700

    Update Notes factory to include journal association

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :note do
-    title { Faker::Lorem.sentence(word_count: rand(1..8)) }
+    # title { Faker::Lorem.sentence(word_count: rand(1..8)) }
     body { Faker::Lorem.paragraph }
+    journal
   end
 end
