11feff17526bb8a78e799724dec2a33d6db138f8
mononoken <ken.maeshima@pm.me>
2023-08-26 17:49:27 -0700

    Add routes for notes

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
     resources :notes, only: %i[new create index]
   end
 
-  resources :notes, only: %i[destroy]
+  resources :notes, only: %i[destroy edit update show]
 end
