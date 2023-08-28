bb1b7e64a2e7df0b6c22ab51f0297bad107b8ee7
mononoken <ken.maeshima@pm.me>
2023-08-28 16:27:46 -0700

    Refactor visit_root_spec to expect user dashboard for authenticated users and remove pending specs

--- spec/system/visit_root_spec.rb
+++ spec/system/visit_root_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.fdescribe "visiting root", type: :system do
+RSpec.describe "visiting root", type: :system do
   let!(:user) { create(:user) }
 
   context "when user is signed out" do
@@ -28,28 +28,8 @@ RSpec.fdescribe "visiting root", type: :system do
       visit root_path
     end
 
-    context "when user has one existing journal" do
-      let(:journal) { create(:journal, user: user) }
-
-      it "redirects to the new note page for that journal" do
-        pending "Method to check for user last journal to route to"
-
-        expect(page).to have_current_path(new_journal_note_path(journal))
-      end
-    end
-
-    context "when user has many existing journals" do
-      it "redirects to the new note page for last visited journal" do
-        pending "Method to store last visited journal"
-
-        expect(page).to have_current_path(new_journal_note_path(:FIX_ME_last_journal))
-      end
-    end
-
-    context "when user has no journals" do
-      it "redirects to new journal path" do
-        expect(page).to have_current_path(new_journal_path)
-      end
+    it "redirects to dashboard path" do
+      expect(page).to have_current_path(dashboard_path)
     end
   end
 end
