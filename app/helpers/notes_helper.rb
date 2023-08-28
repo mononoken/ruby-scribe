332aae47b8e48eb40b25d0703bc54340f84e2ee7
mononoken <ken.maeshima@pm.me>
2023-08-27 22:29:40 -0700

    Create parse_markdown helper using Redcarpet

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,9 @@
+require "redcarpet"
+
 module NotesHelper
+  def parse_markdown(markdown_input)
+    parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+
+    parser.render(markdown_input)
+  end
 end
