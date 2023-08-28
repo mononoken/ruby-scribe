7b93785588eb44bf696f6902fb17a7f11ea38f10
mononoken <ken.maeshima@pm.me>
2023-08-27 18:48:24 -0700

    Create unit test for default_scope

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "dafault_scope" do
+    it "orders notes in descending order" do
+      note1 = create(:note)
+      note2 = create(:note)
+      note3 = create(:note)
+
+      expect(Note.all).to eq([note3, note2, note1])
+    end
+  end
 end
