276e2889b3a919ebbc23b8f93297f1c532070193
mononoken <ken.maeshima@pm.me>
2023-08-28 20:56:30 -0700

    Setup seed for test accounts in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  User.find_or_create_by(email: "iceking@fionafans.com") do |user|
+    user.username = "iceking"
+    user.password = "gunter"
+  end
+
+  User.find_or_create_by(email: "bmo@finncakes.com") do |user|
+    user.username = "BMO"
+    user.password = "football"
+  end
+
+  User.find_or_create_by(email: "finn@human.com") do |user|
+    user.username = "Finn"
+    user.password = "buffbaby"
+  end
+
+  User.find_or_create_by(email: "jake@human.com") do |user|
+    user.username = "Jake the Dog"
+    user.password = "baconpancakes"
+  end
+
+  User.find_or_create_by(email: "vampirequeen@bass.com") do |user|
+    user.username = "Marceline"
+    user.password = "simonp"
+  end
+end
