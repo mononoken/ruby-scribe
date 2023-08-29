75e8766cef0c55d3bc08fb33a4da28902f0b7f7d
mononoken <ken.maeshima@pm.me>
2023-08-28 20:27:57 -0700

    Add username to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_043711) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_234652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,11 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_043711) do
   end
 
   create_table "notes", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "journal_id", null: false
+    t.string "name", default: "", null: false
     t.index ["journal_id"], name: "index_notes_on_journal_id"
   end
 
@@ -39,8 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_043711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "journals", "users"
