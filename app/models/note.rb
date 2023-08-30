ca0c8b836f548dca91a965a33d9c103371bb6ce2
mononoken <ken.maeshima@pm.me>
2023-08-29 21:24:42 -0700

    Add validations and belongs_to association to Note

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,7 @@ class Note < ApplicationRecord
   default_scope { order(updated_at: :desc) }
 
   belongs_to :journal
+  belongs_to :user
+
+  validates :name, presence: true, uniqueness: {scope: :user_id}
 end
