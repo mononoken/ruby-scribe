e44cbe801ee125cb6b86d65982b09ef7c38b9a19
mononoken <ken.maeshima@pm.me>
2023-08-29 17:18:55 -0700

    Add username to Users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
+    username { Faker::Name.first_name + rand(0..99).to_s }
     password { "password" }
   end
 end
