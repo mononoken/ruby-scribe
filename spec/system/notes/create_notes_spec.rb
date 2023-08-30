819c8590df61c6ca583a4a8e898d9af5f1b10579
mononoken <ken.maeshima@pm.me>
2023-08-29 21:23:23 -0700

    Write spec for creating notes matching existing note name

--- spec/system/notes/create_notes_spec.rb
+++ spec/system/notes/create_notes_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe "creating notes", type: :system do
   let!(:user) { create(:user) }
 
-  context "when user with one empty journal is signed in" do
+  before do
+    sign_in user
+  end
+
+  context "when user has one empty journal" do
     let!(:journal) { create(:journal, user: user) }
     let(:note_input) { "Test note" }
-
-    before do
-      sign_in user
-    end
 
     it "shows created journal note after submitting note" do
       visit new_journal_note_path(journal)
@@ -17,6 +17,22 @@ RSpec.describe "creating notes", type: :system do
       click_button "note-save-btn"
 
       expect(page).to have_content(note_input)
+    end
+  end
+
+  context "when user has existing notes" do
+    let!(:journal) { create(:journal, user: user) }
+    let!(:existing_note) { create(:note, journal: journal) }
+    let(:note_count) { Note.all.count }
+
+    before do
+      visit new_journal_note_path(journal)
+      fill_in "name-input", with: existing_note.name
+      click_button "note-save-btn"
+    end
+
+    it "shows error when user tries to create a note with the same name" do
+      expect(page).to have_content("This note name is already in use.")
     end
   end
 end
