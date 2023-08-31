4efc73a40d8c8e3e60aff469836cf6bff523e00a
mononoken <ken.maeshima@pm.me>
2023-08-30 18:31:44 -0700

    Create edit_journals_spec

---
+++ spec/system/journals/edit_journals_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe "editing journals", type: :system do
+  let!(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  context "when user is viewing journal index" do
+    let!(:journal) { create(:journal, user: user) }
+
+    it "has a link to journal's edit page" do
+      visit journals_path
+
+      expect(page).to have_link(href: edit_journal_path(journal))
+    end
+  end
+
+  context "when user is viewing journal notes index" do
+    let!(:journal) { create(:journal, user: user) }
+
+    it "has a link to journal's edit page" do
+      visit journal_notes_path(journal)
+
+      expect(page).to have_link(href: edit_journal_path(journal))
+    end
+  end
+
+  context "when user has an existing journal" do
+    let!(:journal) { create(:journal, user: user) }
+
+    it "shows the edited journal title in the journal index" do
+      visit edit_journal_path(journal)
+      fill_in "journal-name-input", with: "Edited Journal"
+      click_button "journal-save-btn"
+
+      expect(page).to have_text("Edited Journal")
+    end
+  end
+end
