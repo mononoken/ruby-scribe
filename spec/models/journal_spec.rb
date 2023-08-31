89cca082d4a26950a91ed2814b84b6e43a821ebf
mononoken <ken.maeshima@pm.me>
2023-08-30 21:36:06 -0700

    Create a test for journal note dependency

--- spec/models/journal_spec.rb
+++ spec/models/journal_spec.rb
@@ -1,29 +1,44 @@
 require "rails_helper"
 
 RSpec.describe Journal, type: :model do
-  # Delete if failing
-  context "when journal has a name" do
-    subject(:journal) { create(:journal) }
+  describe "#notes" do
+    context "when journal has notes and is deleted" do
+      let!(:journal) { create(:journal) }
+      let!(:note) { create(:note, journal: journal, user: journal.user) }
 
-    context "when journal has a user" do
-      let(:user) { create(:user) }
-
-      subject(:journal) do
-        described_class.new(name: "Test Journal", user:)
-      end
-
-      it "is valid" do
-        expect(journal.valid?).to be(true)
+      # Delete if failing
+      it "destroys all associated notes" do
+        expect { journal.destroy }.to change { Note.count }
+          .by(-1)
       end
     end
   end
 
-  # Delete if failing
-  context "when journal has no name" do
-    subject(:journal) { described_class.new(name: nil) }
+  describe "#valid?" do
+    context "when journal has a name" do
+      subject(:journal) { create(:journal) }
 
-    it "is not valid" do
-      expect(journal.valid?).to be(false)
+      context "when journal has a user" do
+        let(:user) { create(:user) }
+
+        subject(:journal) do
+          described_class.new(name: "Test Journal", user:)
+        end
+
+        # Delete if failing
+        it "is valid" do
+          expect(journal.valid?).to be(true)
+        end
+      end
+    end
+
+    context "when journal has no name" do
+      subject(:journal) { described_class.new(name: nil) }
+
+      # Delete if failing
+      it "is not valid" do
+        expect(journal.valid?).to be(false)
+      end
     end
   end
 end
