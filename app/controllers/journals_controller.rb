32e46ffc8505b48b505150d131bbd37d356ae50a
mononoken <ken.maeshima@pm.me>
2023-08-30 18:32:27 -0700

    Add Journals#edit and #update functionality

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -4,8 +4,7 @@ class JournalsController < ApplicationController
   end
 
   def create
-    @journal = Journal.new(journal_params)
-    @journal.user = current_user
+    @journal = Journal.new(journal_params.merge(user: current_user))
 
     if @journal.save
       redirect_to journal_notes_path(@journal)
@@ -17,6 +16,21 @@ class JournalsController < ApplicationController
 
   def index
     @journals = current_user.journals
+  end
+
+  def edit
+    @journal = Journal.find(params[:id])
+  end
+
+  def update
+    @journal = Journal.find(params[:id])
+
+    if @journal.update(journal_params)
+      redirect_to journal_notes_path(@journal)
+    else
+      flash.now[:error] = @journal.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
