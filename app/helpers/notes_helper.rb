d620db48e59e685b5061d77ab4bb06474c51bc61
mononoken <ken.maeshima@pm.me>
2023-09-03 13:07:16 -0500

    Fixes #26 and adds some settings to Markdown renderer and parserer

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,10 +1,15 @@
 require "redcarpet"
 
 module NotesHelper
-  def parse_markdown(markdown_input)
+  def markdown(input)
     # Redcarpet docs recommend instantiating this object first before use.
-    parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
+    renderer = Redcarpet::Render::HTML.new(
+      filter_html: true, hard_wrap: true
+    )
+    parser = Redcarpet::Markdown.new(renderer,
+      autolink: true, no_intra_emphasis: true)
 
-    parser.render(markdown_input)
+    parser.render(input).html_safe
   end
 end
