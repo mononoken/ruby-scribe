65fb9c16607c58d5ff98a2bfea5f50a2d3da7dc1
mononoken <ken.maeshima@pm.me>
2023-09-17 16:39:09 -0700

    Fixe Collection dependent destroy symbol

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,3 @@
 class Collection < ApplicationRecord
-  has_many :collection_invitations, dependent: destroy
+  has_many :collection_invitations, dependent: :destroy
 end
--- spec/factories/collection_invitations.rb
+++ spec/factories/collection_invitations.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :collection_invitation do
     collection { nil }
     journal { nil }
-    accepted_at { "2023-09-17 16:10:48" }
+    accepted_at { nil }
   end
 end
