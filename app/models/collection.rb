043f818566c54f467583b36c9e53328e33c96164
mononoken <ken.maeshima@pm.me>
2023-09-17 16:22:29 -0700

    Add has_many relations for CollectionInvitation to Collection and Journal

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,3 @@
 class Collection < ApplicationRecord
+  has_many :collection_invitations, dependent: destroy
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,4 +4,5 @@ class Journal < ApplicationRecord
   belongs_to :author, class_name: "User"
 
   has_many :notes, dependent: :destroy
+  has_many :collection_invitations, dependent: :destroy
 end
