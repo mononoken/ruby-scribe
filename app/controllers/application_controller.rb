bf100e9cdc9e9571f6c6d9fea24d386841863174
mononoken <ken.maeshima@pm.me>
2023-09-22 23:11:53 -0700

    Require authorization from all actions by adding requirement to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Use Devise to verify all actions authenticate user
   before_action :authenticate_user!
+
+  # Use Action Policy to verify all actions require authorization
+  verify_authorized
 end
