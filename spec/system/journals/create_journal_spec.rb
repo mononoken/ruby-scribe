2518ffff8337788c0f0c19b55621ca0da6f5a406
mononoken <ken.maeshima@pm.me>
2023-09-27 21:37:20 -0700

    Add new journal link to journal index

--- spec/system/journals/create_journal_spec.rb
+++ spec/system/journals/create_journal_spec.rb
@@ -2,31 +2,40 @@ require "rails_helper"
 
 RSpec.describe "creating journals", type: :system do
   let!(:user) { create(:user) }
+  let(:journal) { build(:journal) }
 
-  context "when user is creating their first journal" do
-    let(:journal) { build(:journal) }
+  before do
+    sign_in user
+  end
 
-    before do
-      sign_in user
-    end
+  it "shows new journal link on journals index" do
+    visit journals_path
 
-    it "shows the new journal title in the journal index" do
+    expect(page).to have_link(href: new_journal_path)
+  end
+
+  it "shows new journal link on dashboard when user has no journals" do
+    visit dashboard_path
+
+    expect(page).to have_link(href: new_journal_path)
+  end
+
+  it "shows the new journal title in the journal index" do
+    visit new_journal_path
+    fill_in "journal-name-input", with: journal.name
+    click_button "journal-save-btn"
+
+    expect(page).to have_text(journal.name)
+  end
+
+  context "when user does not enter a journal title and submits" do
+    let(:error_msg) { "Name can't be blank" }
+
+    it "shows an error" do
       visit new_journal_path
-      fill_in "journal-name-input", with: journal.name
       click_button "journal-save-btn"
 
-      expect(page).to have_text(journal.name)
-    end
-
-    context "when user does not enter a journal title and submits" do
-      let(:error_msg) { "Name can't be blank" }
-
-      it "shows an error" do
-        visit new_journal_path
-        click_button "journal-save-btn"
-
-        expect(page).to have_text(error_msg)
-      end
+      expect(page).to have_text(error_msg)
     end
   end
 end
