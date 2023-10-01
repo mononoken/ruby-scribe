dda8d1bf787bf6752463726a227d18a6ea1c4177
mononoken <ken.maeshima@pm.me>
2023-10-01 01:10:42 -0700

    Add tailwind typography and forms plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,5 @@ export default {
     "./app/assets/stylesheets/**/*.css",
     "./app/javascript/**/*.js",
   ],
-  // plugins: [
-  //   require('@tailwindcss/forms'),
-  //   require('@tailwindcss/aspect-ratio'),
-  //   require('@tailwindcss/typography'),
-  //   require('@tailwindcss/container-queries'),
-  // ]
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
