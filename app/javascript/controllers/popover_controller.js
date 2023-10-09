9a14ed55a85b2e83b66e015683d9005676cced98
mononoken <ken.maeshima@pm.me>
2023-10-09 15:59:17 -0700

    Add sliding animation for hamburger menu

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -2,15 +2,31 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="popover"
 export default class extends Controller {
-  static targets = ["togglable"];
+  static targets = ["togglable", "slideable"];
 
-  initialize() {}
-
-  toggle() {
-    this.togglableTarget.hidden = !this.togglableTarget.hidden;
+  initialize() {
+    this.slideableTarget.classList.add("duration-300", "ease-in-out");
   }
 
-  // popover() {
-  //   document.startViewTransition(() => toggle());
-  // }
+  popover() {
+    this.togglableTarget.hidden ? this.slideIn() : this.slideOut();
+  }
+
+  slideIn() {
+    this.togglableTarget.hidden = !this.togglableTarget.hidden;
+
+    // Timeout required because display is being changed from none
+    setTimeout(() => {
+      this.slideableTarget.classList.toggle("-translate-x-full");
+    }, 100);
+  }
+
+  slideOut() {
+    this.slideableTarget.classList.toggle("-translate-x-full");
+
+    // Timeout required because display is being changed to none
+    setTimeout(() => {
+      this.togglableTarget.hidden = !this.togglableTarget.hidden;
+    }, 1000);
+  }
 }
