6a264881a7f22ca5de5ab576ca0a71c69e507cef
mononoken <ken.maeshima@pm.me>
2023-10-15 15:47:53 -0700

    Update Users factory to create random email with Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :user, aliases: [:author] do
-    sequence(:email) { |n| "user#{n}@example.com" }
+  factory :user, aliases: [:author, :owner] do
+    email { Faker::Internet.email }
     username { Faker::Name.first_name + rand(0..99).to_s }
     password { Faker::Internet.password }
   end
