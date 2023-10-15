22f98da649b95c35db33b19697668f5201980f14
mononoken <ken.maeshima@pm.me>
2023-10-15 15:49:41 -0700

    Create #collection_with_journals to quickly create populated collections

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :collection do
-    
+    name { "#{owner.username}'s Party" }
+    owner
+  end
+end
+
+def collection_with_journals(journals:, owner: journals.first.author, name: "#{owner.username}'s Party")
+  FactoryBot.create(:collection, name:, owner:) do |collection|
+    journals.each do |journal|
+      FactoryBot.create(:invitation, journal:, collection:, accepted_at: DateTime.now)
+    end
   end
 end
