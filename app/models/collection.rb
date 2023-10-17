d6e62386f6c0239e5ee8c9bfe58147ac87431130
mononoken <ken.maeshima@pm.me>
2023-10-17 16:45:38 -0700

    Update Collection and User models for Membership associations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,9 +1,13 @@
 class Collection < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
-  has_many :invitations, dependent: :destroy
-  has_many :journals, through: :invitations
+  has_many :memberships, dependent: :destroy
+  has_many :members, through: :memberships, class_name: "User"
+  # has_many :journals, through: :invitations
+  has_many :journals, through: :memberships
   has_many :authors, through: :journals, class_name: "User"
+  has_many :invitations, dependent: :destroy
 
-  accepts_nested_attributes_for :invitations
+  # accepts_nested_attributes_for :invitations
+  accepts_nested_attributes_for :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
     dependent: :destroy
   has_many :notes, foreign_key: "author_id", inverse_of: "author",
     dependent: :destroy
-  has_many :collections, foreign_key: "owner_id", inverse_of: "owner",
+  has_many :owned_collections, class_name: "Collection",
+    foreign_key: "owner_id", inverse_of: "owner", dependent: :destroy
+  has_many :memberships, foreign_key: "member_id", inverse_of: "member",
+    dependent: :destroy
+  has_many :collections, through: :memberships,
     dependent: :destroy
 end
