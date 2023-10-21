d52e821e956dc16560d1608d72d205f7e6726fc5
mononoken <ken.maeshima@pm.me>
2023-10-20 22:10:31 -0700

    Start Invitation unit test

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,44 @@
 require "rails_helper"
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:invitation) { create(:invitation) }
+
+  describe "#valid?" do
+    context "when duplicate invitation is built" do
+      let(:duplicate_invitation) {
+        build(:invitation, sender: invitation.sender,
+          recipient: invitation.recipient, collection: invitation.collection)
+      }
+
+      # Delete if failing
+      it "is not valid" do
+        expect(duplicate_invitation.valid?).to be(false)
+      end
+    end
+
+    context "when invitation has same recipient and collection but different sender" do
+      let(:duplicate_invitation) {
+        build(:invitation, recipient: invitation.recipient,
+          collection: invitation.collection)
+      }
+
+      # Delete if failing
+      it "is not valid" do
+        expect(duplicate_invitation.valid?).to be(false)
+      end
+    end
+
+    context "when invitation has a different recipient" do
+      let(:new_recipient) { create(:user) }
+      let(:new_invitation) {
+        build(:invitation, sender: invitation.sender,
+          recipient: new_recipient, collection: invitation.collection)
+      }
+
+      # Delete if failing
+      it "is valid" do
+        expect(new_invitation.valid?).to be(true)
+      end
+    end
+  end
 end
