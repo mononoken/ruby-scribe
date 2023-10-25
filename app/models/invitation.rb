dc584bff22a1d0020361654129c35e1c6b488bd4
mononoken <ken.maeshima@pm.me>
2023-10-25 02:48:26 -0700

    Create Invitation#accept

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,4 +4,10 @@ class Invitation < ApplicationRecord
   belongs_to :collection
 
   validates :collection, uniqueness: {scope: :recipient, message: "User has already been invited to this collection."}
+
+  def accept
+    update(accepted_at: DateTime.now)
+
+    Membership.create(collection: collection, member: recipient)
+  end
 end
