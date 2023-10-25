444879d0a0c09c481bfbf2899e68028bc6087a19
mononoken <ken.maeshima@pm.me>
2023-10-25 02:45:15 -0700

    Make Membership journal association optional

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,7 +3,6 @@ class Collection < ApplicationRecord
 
   has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, class_name: "User"
-  # has_many :journals, through: :invitations
   has_many :journals, through: :memberships
   has_many :authors, through: :journals, class_name: "User"
   has_many :invitations, dependent: :destroy
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,5 @@
 class Membership < ApplicationRecord
   belongs_to :collection
   belongs_to :member, class_name: "User"
-  belongs_to :journal
+  belongs_to :journal, optional: true
 end
