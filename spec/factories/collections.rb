2eb908c809cb2073e6afd790787e820aa268ec5d
mononoken <ken.maeshima@pm.me>
2023-10-27 23:51:42 -0700

    Create #collection_with_members factory method

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -5,6 +5,16 @@ FactoryBot.define do
   end
 end
 
+def collection_with_members(members:, owner: FactoryBot.create(:user),
+  name: "#{owner.username}'s Party")
+  FactoryBot.create(:collection, name:, owner:) do |collection|
+    members.each do |member|
+      FactoryBot.create(:membership, member: member,
+        collection:)
+    end
+  end
+end
+
 def collection_with_journals(journals:, owner: journals.first.author,
   name: "#{owner.username}'s Party")
   FactoryBot.create(:collection, name:, owner:) do |collection|
