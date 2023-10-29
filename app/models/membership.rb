ee34173bb7365e847b6f331f68af95fde69a115e
mononoken <ken.maeshima@pm.me>
2023-10-29 01:52:32 -0700

    Add role to Membership as enum

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,5 +3,7 @@ class Membership < ApplicationRecord
   belongs_to :member, class_name: "User"
   belongs_to :journal, optional: true
 
+  enum :role, {owner: 0, admin: 1, member: 2}
+
   validates :member, uniqueness: {scope: :collection, message: "has already joined this collection."}
 end
