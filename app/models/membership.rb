a311fb9dbc45479569d9cff6083ba8b134314916
mononoken <ken.maeshima@pm.me>
2023-10-29 23:26:07 -0700

    Add validation to Membership for member must own journal they are adding

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,5 +5,18 @@ class Membership < ApplicationRecord
 
   enum :role, {owner: 0, admin: 1, member: 2}
 
-  validates :member, uniqueness: {scope: :collection, message: "has already joined this collection."}
+  validates :member, uniqueness:
+    {scope: :collection, message: "has already joined this collection."}
+
+  validate :member_owns_journal
+
+  private
+
+  def member_owns_journal
+    return unless journal
+
+    if journal.author != member
+      errors.add(:journal, "is not authored by this member")
+    end
+  end
 end
