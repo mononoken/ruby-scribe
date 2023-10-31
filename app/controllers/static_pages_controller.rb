916ba601c83e80031389182e8a2bbced1536f105
mononoken <ken.maeshima@pm.me>
2023-10-30 22:46:10 -0700

    Add about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!, except: %i[splash]
-  skip_verify_authorized only: %i[splash]
+  before_action :authenticate_user!, except: %i[splash about]
+  skip_verify_authorized only: %i[splash about]
 
   def splash
+  end
+
+  def about
   end
 end
