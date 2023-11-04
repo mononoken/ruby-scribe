dbae01e6c0868cdcb9d8612dce8ea1309623cc0a
mononoken <ken.maeshima@pm.me>
2023-11-04 01:27:14 -0700

    Add desc scope for ordering comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :note
   belongs_to :author, class_name: "User"
+
+  scope :desc, -> { order(created_at: :desc) }
+
+  validates :body, length: {minimum: 2}
 end
