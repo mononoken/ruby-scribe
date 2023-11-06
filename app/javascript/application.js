a98805b45493bf77c8a1544d37bd7265b1245327
mononoken <ken.maeshima@pm.me>
2023-11-06 02:12:10 -0800

    Add turbo-view-transitions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
+
+// View transitions with Turbo
+// https://github.com/palkan/turbo-view-transitions
+import {
+  shouldPerformTransition,
+  performTransition,
+} from "turbo-view-transitions";
+
+document.addEventListener("turbo:before-render", (event) => {
+  if (shouldPerformTransition()) {
+    event.preventDefault();
+
+    performTransition(document.body, event.detail.newBody, async () => {
+      await event.detail.resume();
+    });
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  // View Transitions don't play nicely with Turbo cache
+  if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
