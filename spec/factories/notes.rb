93688411721387d652c7c5831b1106cd34128e39
mononoken <ken.maeshima@pm.me>
2023-11-11 20:53:07 -0800

    Refactor Note Factory author association to infer journal author if not given

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     name { Faker::Lorem.sentence(word_count: rand(1..4)) }
     body { Faker::Lorem.paragraph }
-    author
+    author { @overrides[:journal]&.author || association(:author) }
     journal { association :journal, author: author }
   end
 end
