83cbeb1d64d4a1d5f329aa06c7355244ae331f29
mononoken <ken.maeshima@pm.me>
2023-11-12 04:40:51 -0800

    Implement FaeScribeRenderer for markdown rendering internal links

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,10 +1,51 @@
 require "redcarpet"
 
 module NotesHelper
+  class FaeScribeRenderer < Redcarpet::Render::HTML
+    def paragraph(text)
+      process_custom_tags("<p>#{text.strip}</p>\n")
+    end
+
+    def list_item(text, params)
+      "<li>#{process_custom_tags(text)}</li>\n"
+    end
+
+    def header(text, level)
+      "<h#{level}>#{process_custom_tags(text)}</h#{level}>"
+    end
+
+    def internal_link(note_name)
+      note = Note.find_by(name: note_name)
+      return "[[#{note_name}]]" unless note
+
+      url = Rails.application.routes.url_helpers.note_path(note)
+      title = note.name
+      content = note.name
+
+      link(url, title, content)
+    end
+
+    def link(link, title, content)
+      "<a href='#{link}'>#{content}</a>"
+    end
+
+    private
+
+    def process_custom_tags(text)
+      matches = text.to_enum(:scan, /\[\[(.*?)\]\]/).map { Regexp.last_match }
+
+      if matches.any?
+        matches.each { |match| text.sub!(match[0], internal_link(match[1])) }
+      end
+
+      text
+    end
+  end
+
   def markdown(input)
-    # Redcarpet docs recommend instantiating this object first before use.
+    # Redcarpet docs recommend instantiating renderer object first before use.
     options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
-    renderer = Redcarpet::Render::HTML.new(
+    renderer = FaeScribeRenderer.new(
       filter_html: true, hard_wrap: true
     )
     parser = Redcarpet::Markdown.new(renderer,
