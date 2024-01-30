5eaafb636369dcc20e97d73cb5af8e3c1dc4286f
mononoken <ken.maeshima@pm.me>
2024-01-29 18:52:31 -0800

    Update config for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,21 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # If a default host is specifically defined then it's used otherwise the app is
+  # assumed to be a Heroku review app. Note that `Hash#fetch` is used defensively
+  # so the app will blow up at boot-time if both `DEFAULT_URL_HOST` and
+  # `HEROKU_APP_NAME` aren't defined.
+  host = ENV["DEFAULT_URL_HOST"] ||
+    "#{ENV.fetch("HEROKU_APP_NAME")}.herokuapp.com"
+
+  # Set the correct protocol as SSL isn't configured in development or test.
+  protocol = Rails.application.config.force_ssl ? "https" : "http"
+
+  config.action_controller.default_url_options = {
+    host: host,
+    protocol: protocol
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fae_scribe_production"
