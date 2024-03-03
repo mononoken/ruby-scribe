23a48243c53bdc96fa2b361c921971cb4a0286ff
mononoken <ken.maeshima@pm.me>
2024-03-03 15:35:58 -0800

    Create first counter spec for adding them to notes

---
+++ spec/system/notes/add_counter_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Counter", type: :system do
+  let!(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  context "when user has existing note" do
+    let!(:note) { create(:note, author: user) }
+    let(:counter) { build(:counter) }
+
+    it "shows button to add counter" do
+      visit note_path(note)
+      click_button "counter-create-btn"
+
+      expect(page).to have_content("New counter")
+    end
+
+    context "when incrementable exists" do
+      # let!(:incrementable) { create(:incrementable, note:) }
+
+      it "should have button to increment"
+      it "should have button to decrement"
+    end
+  end
+end
