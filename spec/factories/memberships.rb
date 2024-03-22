413622b05df273ef5fd9346621ba7d29a21af3b6
mononoken <ken.maeshima@pm.me>
2024-03-21 20:28:34 -0700

    Refactor membership factory to assign member based on journal if given

--- spec/factories/memberships.rb
+++ spec/factories/memberships.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :membership do
-    collection { nil }
-    member { nil }
+    collection
     journal { nil }
+    member {
+      if journal
+        journal.author
+      else
+        association :user
+      end
+    }
     role { :member }
   end
 end
