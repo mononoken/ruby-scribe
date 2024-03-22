5b9ac2ace1543c991df7999bf6161d297a46b9ef
mononoken <ken.maeshima@pm.me>
2024-03-21 20:28:02 -0700

    Begin to flesh out note policy spec

--- spec/policies/note_policy_spec.rb
+++ spec/policies/note_policy_spec.rb
@@ -1,14 +1,36 @@
 require "rails_helper"
 
 RSpec.describe NotePolicy, type: :policy do
-  # See https://actionpolicy.evilmartians.io/#/testing?id=rspec-dsl
+  # let(:user) { create(:user) }
+  # let(:record) { create(:note) }
   #
-  # let(:user) { build_stubbed :user }
-  # let(:record) { build_stubbed :post, draft: false }
-  # let(:context) { {user: user} }
-
-  # describe_rule :index? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
+  # describe :index? do
+  #   let(:policy) { NotePolicy.new(record, user: user, journal: record.journal) }
+  #
+  #   subject { policy.apply(:index?) }
+  #
+  #   context "when user is allowed to show journal" do
+  #     before do
+  #       allow(policy).to receive(:allowed_to?)
+  #         .with(:show?, record.journal)
+  #         .and_return(true)
+  #     end
+  #
+  #     it "returns true" do
+  #       is_expected.to be(true)
+  #     end
+  #   end
+  #
+  #   context "when user is not allowed to show journal" do
+  #     before do
+  #       allow(policy).to receive(:allowed_to?)
+  #         .and_return(false)
+  #     end
+  #
+  #     it "returns false" do
+  #       is_expected.to be(false)
+  #     end
+  #   end
   # end
   #
   # describe_rule :create? do
@@ -17,5 +39,40 @@ RSpec.describe NotePolicy, type: :policy do
   #
   # describe_rule :manage? do
   #   pending "add some examples to (or delete) #{__FILE__}"
+  # end
+  #
+  # describe :show? do
+  #   let(:policy) { NotePolicy.new(record, user: user) }
+  #
+  #   subject { policy.apply(:show?) }
+  #
+  #   context "when user is note's author" do
+  #     let(:user) { record.author }
+  #
+  #     it "returns true" do
+  #       is_expected.to be(true)
+  #     end
+  #   end
+  #
+  #   context "when user is not author" do
+  #     context "when user has membership to same collection as note's journal" do
+  #       before do
+  #         @journal_membership = create(:membership,
+  #           journal: record.journal)
+  #         @user_membership = create(:membership,
+  #           member: user, collection: @journal_membership.collection)
+  #       end
+  #
+  #       it "returns true" do
+  #         is_expected.to be(true)
+  #       end
+  #     end
+  #
+  #     context "when user is not a member of the journal's collection" do
+  #       it "returns false" do
+  #         is_expected.to be(false)
+  #       end
+  #     end
+  #   end
   # end
 end
