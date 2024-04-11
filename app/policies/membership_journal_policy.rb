384518f544637d72cb7f8ea3c4b9dedeff4e07c5
mononoken <ken.maeshima@pm.me>
2024-04-11 01:57:30 -0700

    Add specs for MembershipJournalPolicy

--- app/policies/membership_journal_policy.rb
+++ app/policies/membership_journal_policy.rb
@@ -1,4 +1,10 @@
 class MembershipJournalPolicy < ApplicationPolicy
+  authorize :campaign, optional: true
+
+  def create?
+    member?
+  end
+
   def destroy?
     allow! if owner?
 
@@ -13,5 +19,12 @@ class MembershipJournalPolicy < ApplicationPolicy
 
   def author?
     user.id == record.journal.author_id
+  end
+
+  def member?
+    member_ids = campaign.members.map(&:id)
+    member_ids.any? do |member_id|
+      user.id == member_id
+    end
   end
 end
--- spec/policies/membership_journal_policy_spec.rb
+++ spec/policies/membership_journal_policy_spec.rb
@@ -1,21 +1,44 @@
 require "rails_helper"
 
 RSpec.describe MembershipJournalPolicy, type: :policy do
-  # See https://actionpolicy.evilmartians.io/#/testing?id=rspec-dsl
-  #
-  # let(:user) { build_stubbed :user }
-  # let(:record) { build_stubbed :post, draft: false }
-  # let(:context) { {user: user} }
+  let(:user) { build_stubbed :user }
+  let(:record) { create :membership_journal }
+  let(:context) { {user:} }
 
-  describe_rule :index? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe "new?" do
+    let(:policy) { described_class.new(record, user:) }
+
+    it "is an alias of :create? rule" do
+      expect(:new?).to be_an_alias_of(policy, :create?)
+    end
   end
 
   describe_rule :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:context) { {user:, campaign: record.campaign} }
+
+    succeed "when user is owner of campaign" do
+      let(:user) { record.campaign.owner }
+    end
+
+    succeed "when user is member of campaign" do
+      let(:user_membership) {
+        create :membership, campaign: record.campaign, member: user
+      }
+      let(:user) { record.campaign.owner }
+    end
+
+    failed "when user is not member of campaign"
   end
 
-  describe_rule :manage? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe_rule :destroy? do
+    succeed "when user is owner" do
+      let(:user) { record.campaign.owner }
+    end
+
+    succeed "when member is journal author" do
+      let(:user) { record.journal.author }
+    end
+
+    failed "when member is not journal author"
   end
 end
