2ce7d07f2f0106279a25c3aace2c569dfd68d687
mononoken <ken.maeshima@pm.me>
2024-04-11 01:57:56 -0700

    Implement MembershipJournalPolicy in controller

--- app/controllers/membership_journals_controller.rb
+++ app/controllers/membership_journals_controller.rb
@@ -1,14 +1,14 @@
 class MembershipJournalsController < ApplicationController
-  skip_verify_authorized # TEMP FIX_ME
+  before_action :set_campaign, only: %i[new create]
+  before_action :context_authorize!, only: %i[new create]
+  before_action :set_membership_journal, only: %i[destroy]
 
   def new
-    @campaign = Campaign.find(params[:campaign_id])
     @membership_journal = MembershipJournal.new
     @journals = current_user.journals
   end
 
   def create
-    @campaign = Campaign.find(params[:campaign_id])
     membership = Membership.find_by(campaign: @campaign, member: current_user)
     @membership_journal = MembershipJournal.new(
       membership_journal_params.merge(membership_id: membership.id)
@@ -23,21 +23,32 @@ class MembershipJournalsController < ApplicationController
   end
 
   def destroy
-    @membership_journal = MembershipJournal.find(params[:id])
-    authorize! @membership_journal
-
     @membership_journal.destroy
 
     flash[:success] = "You successfully removed #{@membership_journal.journal.name}."
     redirect_to edit_campaign_path(@membership_journal.campaign)
-  rescue ActionPolicy::Unauthorized
-    flash[:error] = "You do not have permission to remove that journal."
-    redirect_to edit_campaign_path(@membership_journal.campaign), status: :forbidden
   end
 
   private
 
   def membership_journal_params
     params.require(:membership_journal).permit(:membership_id, :journal_id)
+  end
+
+  def set_membership_journal
+    @membership_journal = MembershipJournal.find(params[:id])
+
+    authorize! @membership_journal
+  rescue ActionPolicy::Unauthorized
+    flash[:error] = "You do not have permission to do that action."
+    redirect_to campaign_path(@membership_journal.campaign), status: :forbidden
+  end
+
+  def set_campaign
+    @campaign = Campaign.find(params[:campaign_id])
+  end
+
+  def context_authorize!
+    authorize! MembershipJournal, context: {campaign: @campaign}
   end
 end
