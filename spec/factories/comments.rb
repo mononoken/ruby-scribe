d71e85a31984cf835610ce78e5539a3c71ec6798
mononoken <ken.maeshima@pm.me>
2024-04-29 21:03:20 -0700

    Fix comment author association so that comment author matches note author by default

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.sentence(word_count: rand(1..10)) }
     note
-    author
+    author { @overrides[:note]&.author || association(:author) }
   end
 end
