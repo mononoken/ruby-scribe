fc38e7d6b60981a4860010c28036c08742ec0ac1
mononoken <ken.maeshima@pm.me>
2024-04-29 18:01:52 -0700

    Create JournalPolicy spec

--- spec/policies/journal_policy_spec.rb
+++ spec/policies/journal_policy_spec.rb
@@ -1,21 +1,61 @@
 require "rails_helper"
 
 RSpec.describe JournalPolicy, type: :policy do
-  # See https://actionpolicy.evilmartians.io/#/testing?id=rspec-dsl
-  #
-  # let(:user) { build_stubbed :user }
-  # let(:record) { build_stubbed :post, draft: false }
-  # let(:context) { {user: user} }
+  let(:user) { build_stubbed :user }
+  let(:record) { create :journal }
+  let(:context) { {user:} }
 
-  # describe_rule :index? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
-  #
-  # describe_rule :create? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
-  #
-  # describe_rule :manage? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
+  describe_rule "new?" do
+    succeed "when user is present"
+
+    # This is authentication and implemented via Devise
+    # failed "when is not present"
+  end
+
+  describe_rule :create? do
+    succeed "when user is author" do
+      let(:user) { record.author }
+    end
+
+    failed "when user is not author"
+  end
+
+  describe_rule "index?" do
+    succeed "when user is present"
+
+    # This is authentication and implemented via Devise
+    # failed "when is not present"
+  end
+
+  describe_rule :manage? do
+    succeed "when user is author" do
+      let(:user) { record.author }
+    end
+
+    failed "when user is not journal author"
+  end
+
+  describe "edit?" do
+    let(:policy) { described_class.new(record, user:) }
+
+    it "is an alias of :manage? rule" do
+      expect(:edit?).to be_an_alias_of(policy, :manage?)
+    end
+  end
+
+  describe "update?" do
+    let(:policy) { described_class.new(record, user:) }
+
+    it "is an alias of :manage? rule" do
+      expect(:update?).to be_an_alias_of(policy, :manage?)
+    end
+  end
+
+  describe "destroy?" do
+    let(:policy) { described_class.new(record, user:) }
+
+    it "is an alias of :manage? rule" do
+      expect(:destroy?).to be_an_alias_of(policy, :manage?)
+    end
+  end
 end
