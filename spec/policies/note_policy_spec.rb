c3dad07df6509b9faabb2763accc918d9775a77e
mononoken <ken.maeshima@pm.me>
2024-04-29 20:03:19 -0700

    Add NotePolicy spec

--- spec/policies/note_policy_spec.rb
+++ spec/policies/note_policy_spec.rb
@@ -1,78 +1,87 @@
 require "rails_helper"
 
 RSpec.describe NotePolicy, type: :policy do
-  # let(:user) { create(:user) }
-  # let(:record) { create(:note) }
-  #
-  # describe :index? do
-  #   let(:policy) { NotePolicy.new(record, user: user, journal: record.journal) }
-  #
-  #   subject { policy.apply(:index?) }
-  #
-  #   context "when user is allowed to show journal" do
-  #     before do
-  #       allow(policy).to receive(:allowed_to?)
-  #         .with(:show?, record.journal)
-  #         .and_return(true)
-  #     end
-  #
-  #     it "returns true" do
-  #       is_expected.to be(true)
-  #     end
-  #   end
-  #
-  #   context "when user is not allowed to show journal" do
-  #     before do
-  #       allow(policy).to receive(:allowed_to?)
-  #         .and_return(false)
-  #     end
-  #
-  #     it "returns false" do
-  #       is_expected.to be(false)
-  #     end
-  #   end
-  # end
-  #
-  # describe_rule :create? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
-  #
-  # describe_rule :manage? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
-  #
-  # describe :show? do
-  #   let(:policy) { NotePolicy.new(record, user: user) }
-  #
-  #   subject { policy.apply(:show?) }
-  #
-  #   context "when user is note's author" do
-  #     let(:user) { record.author }
-  #
-  #     it "returns true" do
-  #       is_expected.to be(true)
-  #     end
-  #   end
-  #
-  #   context "when user is not author" do
-  #     context "when user has membership to same campaign as note's journal" do
-  #       before do
-  #         @journal_membership = create(:membership,
-  #           journal: record.journal)
-  #         @user_membership = create(:membership,
-  #           member: user, campaign: @journal_membership.campaign)
-  #       end
-  #
-  #       it "returns true" do
-  #         is_expected.to be(true)
-  #       end
-  #     end
-  #
-  #     context "when user is not a member of the journal's campaign" do
-  #       it "returns false" do
-  #         is_expected.to be(false)
-  #       end
-  #     end
-  #   end
-  # end
+  let(:user) { build_stubbed :user }
+  let(:record) { create :note }
+  let(:context) { {user:} }
+
+  describe "new?" do
+    it "is an alias of :create? rule" do
+      expect(:new?).to be_an_alias_of(policy, :create?)
+    end
+  end
+
+  describe_rule :create? do
+    let(:context) { {user:, journal: record.journal} }
+
+    succeed "when user is allowed to manage the note's journal" do
+      before do
+        allow(policy).to receive(:allowed_to?)
+          .with(:manage?, record.journal)
+          .and_return(true)
+      end
+    end
+
+    failed "when user is not allowed to manage the note's journal" do
+      before do
+        allow(policy).to receive(:allowed_to?)
+          .with(:manage?, record.journal)
+          .and_return(false)
+      end
+    end
+  end
+
+  describe_rule "index?" do
+    let(:context) { {user:, journal: record.journal} }
+
+    succeed "when user is allowed to manage the note's journal" do
+      before do
+        allow(policy).to receive(:allowed_to?)
+          .with(:manage?, record.journal)
+          .and_return(true)
+      end
+    end
+
+    failed "when user is not allowed to manage the note's journal" do
+      before do
+        allow(policy).to receive(:allowed_to?)
+          .with(:manage?, record.journal)
+          .and_return(false)
+      end
+    end
+  end
+
+  describe_rule :manage? do
+    succeed "when user is author" do
+      let(:user) { record.author }
+    end
+
+    failed "when user is not author"
+  end
+
+  describe "edit?" do
+    let(:policy) { described_class.new(record, user:) }
+
+    it "is an alias of :manage? rule" do
+      expect(:edit?).to be_an_alias_of(policy, :manage?)
+    end
+  end
+
+  describe "update?" do
+    it "is an alias of :manage? rule" do
+      expect(:update?).to be_an_alias_of(policy, :manage?)
+    end
+  end
+
+  describe "destroy?" do
+    it "is an alias of :manage? rule" do
+      expect(:destroy?).to be_an_alias_of(policy, :manage?)
+    end
+  end
+
+  describe "show?" do
+    it "is an alias of :manage? rule" do
+      expect(:show?).to be_an_alias_of(policy, :manage?)
+    end
+  end
 end
