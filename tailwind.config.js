e033cff2ce2f8b369860ce682dfb72c27bab8c00
mononoken <ken.maeshima@pm.me>
2024-05-09 00:28:18 -0700

    Add mauve color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        purplehaze: "#93628f",
+        mauve: {
+          50: "#fbf8fa",
+          100: "#f6f0f7",
+          200: "#eee0ee",
+          300: "#dfc8de",
+          400: "#cba7c8",
+          500: "#b382b1",
+          600: "#93628f",
+          700: "#7b5077",
+          800: "#654361",
+          900: "#553a52",
+          950: "#341e32",
+        },
         // primary: "#FDFFFC", tailwind: white
         // base: "0E1116", tailwind: neutral-900
         // secondary: stone-400 (gray)
         // alert: #AA4465, tailwind: pink-700
         // active: #822E81, tailwind: fuchsia-800
-        //
-        // abandon? dark purple: "#9E7B9B", tailwind: none closest is stone-400 (gray)
-        // abandon? mauve: "#CB9CF2", tailwind: violet-300
-        // abandon? lightning yellow: "#FABC2A", tailwind: amber-400
       },
     },
   },
