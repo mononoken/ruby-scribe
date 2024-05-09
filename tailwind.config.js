19bd55303092d64b5dc9ac58637a610913da1125
mononoken <ken.maeshima@pm.me>
2024-05-08 23:56:08 -0700

    Change static pages to new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ export default {
     extend: {
       colors: {
         purplehaze: "#93628f",
+        // primary: "#FDFFFC", tailwind: white
+        // base: "0E1116", tailwind: neutral-900
+        // secondary: stone-400 (gray)
+        // alert: #AA4465, tailwind: pink-700
+        // active: #822E81, tailwind: fuchsia-800
+        //
+        // abandon? dark purple: "#9E7B9B", tailwind: none closest is stone-400 (gray)
+        // abandon? mauve: "#CB9CF2", tailwind: violet-300
+        // abandon? lightning yellow: "#FABC2A", tailwind: amber-400
       },
     },
   },
