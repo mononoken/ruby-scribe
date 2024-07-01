523af4ac7f1ce899b715e80387538920b23d5d5d
mononoken <ken.maeshima@pm.me>
2024-06-30 22:39:16 -0700

    Replace zinc with slate

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,19 +25,6 @@ export default {
           900: "#553a52",
           950: "#341e32",
         },
-        zinc: {
-          50: "#fafafa",
-          100: "#f4f4f5",
-          200: "#e4e4e7",
-          300: "#d4d4d8",
-          400: "#a1a1aa",
-          500: "#71717a",
-          600: "#52525b",
-          700: "#3f3f46",
-          800: "#27272a",
-          900: "#18181b",
-          950: "#09090b",
-        },
         // primary: "#FDFFFC", tailwind: white
         // base: "0E1116", tailwind: neutral-900
         // secondary: stone-400 (gray)
