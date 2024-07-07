9a997f3c663f8acde1043c1cc59fbb5eddc3c041
mononoken <ken.maeshima@pm.me>
2024-07-06 23:26:50 -0700

    Remove content from Users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,2 @@
 class UsersController < ApplicationController
-  def show
-    @invitations = Invitation.pending.where(recipient_id: current_user.id)
-    authorize! context: {invitations: @invitations}
-  end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,2 @@
 class UserPolicy < ApplicationPolicy
-  authorize :invitations, optional: true
-
-  def show?
-    allowed_to?(:index?, invitations, namespace: Users)
-  end
 end
